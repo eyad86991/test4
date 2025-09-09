4ab6880f1b61a87d952fd6413f8439d11610673e
eyad86991 <123450424+eyad86991@users.noreply.github.com>
2025-09-09 21:18:23 +0300

    Time: 0 ms (100%), Space: 7.7 MB (87.81%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) { 
+        if (n == 1) return 1;
+        if(n== 2) return 2;
+    int first = 1, second = 2;
+    int currentNumber;
+    for (int i = 3; i <= n; i++) {
+        currentNumber = first + second;
+        first = second;
+        second = currentNumber;
+    }
+    return currentNumber;
+}
+
+
+};
